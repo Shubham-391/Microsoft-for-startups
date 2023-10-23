3104961d34c985b2ec9c6653b90d75de8cdad4bc
Shubham-391 <subhamy1234@gmail.com>
2023-10-23 09:16:42 +0530

    preloader, backtotop, animations

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -2,6 +2,15 @@ let view = document.querySelector(".view");
 let span1 = document.querySelector(".span1");
 let span2 = document.querySelector(".span2");
 let span3 = document.querySelector(".span3");
+// preloader
+const preloader = document.getElementById("preloader")
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("d-none");
+    document.body.classList.remove("overflow-hidden")
+}, 2000)
+
+// navbar
+
 function navbar() {
     view.classList.toggle("end-0");
     span1.classList.toggle("crossLine1");
@@ -17,10 +26,13 @@ function navbar2() {
     document.querySelector("body").classList.remove("overflow-hidden");
 };
 
+// slick slider
+
 $('.section5-slick-slider').slick({
-    prevArrow: '.prev',
-    nextArrow: '.next',
+    prevArrow: '.prev2',
+    nextArrow: '.next2',
     autoplay: true,
+    fade: true,
     responsive: [
         {
             breakpoint: 768,
@@ -29,6 +41,29 @@ $('.section5-slick-slider').slick({
             }
         }
     ]
+});
+
+// back to top
+
+let topIcon = document.getElementById("top");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        topIcon.style.display = "flex";
+    }
+    else {
+        topIcon.style.display = "none";
+    }
+});
+
+function up() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+// aos
+
+AOS.init({
+    once: true,
 });
 
 
